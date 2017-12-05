370e34afe88f883f0a4f204ee6700a7a5b4813cc
Tyler Holmes <contact@tylerholmes.com>
2017-12-04 23:37:49 -0800

    Adds database project with two sample tables.

---
+++ Database/Tables/Upload.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Upload]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] INT NOT NULL, 
+    [Payload] TEXT NULL, 
+    [Created] DATETIME NOT NULL DEFAULT getdate(), 
+    [Updated] DATETIME NOT NULL DEFAULT getdate(), 
+    CONSTRAINT [FK_Upload_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
